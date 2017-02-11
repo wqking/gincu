08221f2bb59505e747ff072b663d9829af14295c
Wang Qi <wqking@outlook.com>
2017-02-11 22:54:44 +0800

    Improved Android build batch file.

--- build/android/build.bat
+++ build/android/build.bat
@@ -1,3 +1,16 @@
+@echo off
+
 call ndk-build
+if errorlevel 1 goto err
+
 call ant %1
+if errorlevel 1 goto err
 call sh sign.sh
+if errorlevel 1 goto err
+
+goto end
+
+:err
+echo Error occurred!
+
+:end
